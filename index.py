73e58806f381b2617961e792128cb65cb332c765
Cleiton <Cleitoncampos2015@gmail.com>
2019-06-18 17:05:29 -0300

    primeiro codigo em python

---
+++ index.py
@@ -0,0 +1,49 @@
+# # # oi = 1
+
+# # # print(oi)
+# # # cachorro = { 'Nomes dos dogs': ['Tottó',' Fubá',' pulginha',' Cleiton',' Godofredo',' Astolfo'], 'Raça': 'Vira lata'}
+# # # print(cachorro['Nomes dos dogs'])
+
+# # #!python3
+
+# # idade = 20 
+
+# # if idade > 18:
+
+# #     print ('maior que idade')
+
+# # elif idade == 18:
+
+# #     print('18 anos')
+# # else:
+# #     print('menor')
+
+# # while idade < 20: 
+
+# #     print ('menor ')
+
+# #     idade = idade + 1
+
+# #     nome = 'Eric'
+
+# #     n_trab = 40
+
+# #     valor_hora = 
+
+
+# oi = ["Carro" ,"moto","aviao"]
+
+# print(oi)
+
+
+nome = 'Cleiton'
+n_trabalho = 40
+valor_hora = 10
+
+salario = n_trabalho * valor_hora
+
+print(nome, salario)
+
+if salario > 998,00
+
+
