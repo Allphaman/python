9e64c5886b81c7e13880b27af03d6fcbf9205e52
Cleiton <Cleitoncampos2015@gmail.com>
2019-06-19 17:06:02 -0300

    adicionar POO

--- index.py
+++ index.py
@@ -1,49 +1,80 @@
-# # # oi = 1
+# # # # # oi = 1
+
+# # # # # print(oi)
+# # # cachorro = { ['Nomes dos dogs': ['Tottó',' Fubá',' pulginha',' Cleiton',' Godofredo',' Astolfo'],
+
+# # #  'Raça': 'Vira lata','Pastor alemão','pug','labrador'],}
+
+# # def exibir( param1, param2):
+        
+# #         print(param1, param2)
+
+# #     def exibir("oi", "Cleiton")
+
+
+
+
+
+
+
+# # # # #!python3
+
+# # # # idade = 20 
+
+# # # # if idade > 18:
+
+# # # #     print ('maior que idade')
+
+# # # # elif idade == 18:
+
+# # # #     print('18 anos')
+# # # # else:
+# # # #     print('menor')
+
+# # # # while idade < 20: 
+
+# # # #     print ('menor ')
+
+# # # #     idade = idade + 1
+
+# # # #     nome = 'Eric'
+
+# # # #     n_trab = 40
+
+# # # #     valor_hora = 
+
+
+# # # oi = ["Carro" ,"moto","aviao"]
 
 # # # print(oi)
-# # # cachorro = { 'Nomes dos dogs': ['Tottó',' Fubá',' pulginha',' Cleiton',' Godofredo',' Astolfo'], 'Raça': 'Vira lata'}
-# # # print(cachorro['Nomes dos dogs'])
-
-# # #!python3
-
-# # idade = 20 
-
-# # if idade > 18:
-
-# #     print ('maior que idade')
-
-# # elif idade == 18:
-
-# #     print('18 anos')
-# # else:
-# #     print('menor')
-
-# # while idade < 20: 
-
-# #     print ('menor ')
-
-# #     idade = idade + 1
-
-# #     nome = 'Eric'
-
-# #     n_trab = 40
-
-# #     valor_hora = 
 
 
-# oi = ["Carro" ,"moto","aviao"]
+# # # nome = 'Cleiton'
+# # # n_trabalho = 40
+# # # valor_hora = 10
 
-# print(oi)
+# # # salario = n_trabalho * valor_hora
+
+# # # print(nome, salario)
+
+# # # if salario > 998,00
+
+class Animal():
+
+    def__init__(self, nome, perna, especie):
+    
 
 
-nome = 'Cleiton'
-n_trabalho = 40
-valor_hora = 10
 
-salario = n_trabalho * valor_hora
-
-print(nome, salario)
-
-if salario > 998,00
+# usuario = input("Digite seu nome: ")
+# senha = int(input("Digite sua senha:"))
 
 
+# if type(usuario) == str and type(senha) == int:
+
+#     print("Deu acerto")
+
+# else:
+#     print("deu errado")
+
+    
